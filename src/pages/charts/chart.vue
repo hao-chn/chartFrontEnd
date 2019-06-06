5a841e08beef1c1200c6c3785b390e88ee84899b
Mr.hao <hao.chn@qq.com>
2019-06-06 18:45:26 +0800

    ce

--- src/pages/charts/chart.vue
+++ src/pages/charts/chart.vue
@@ -270,6 +270,28 @@
                 </el-col>
             </el-row>
         </el-card>
+        <!-- 模型KS值 -->
+        <el-card class="box-card" id="chartAB" style="margin-bottom: 30px">
+            <el-table
+                :data="tableData"
+                border
+                style="width: 100%">
+                <el-table-column
+                prop="date"
+                label="日期"
+                width="180">
+                </el-table-column>
+                <el-table-column
+                prop="name"
+                label="姓名"
+                width="180">
+                </el-table-column>
+                <el-table-column
+                prop="address"
+                label="地址">
+                </el-table-column>
+            </el-table>
+        </el-card>
     </div>
 </template>
 
@@ -369,6 +391,23 @@
                 overdueMonth:{},
                 monthList:[],
                 seriesMon:[],
+                tableData: [{
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                    }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                    }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                    }, {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }]
             }
         },
         updated(){
@@ -379,6 +418,7 @@
                 url: '/home',
                 baseURL: process.env.API_BASEURL,
             }).then((res) => {
+                console.log(res,'385')
                 this.productNames = res.data[0].productName;
                 this.channelIds = res.data[0].channelId;
                 this.scoreNames = res.data[0].scoreName;
@@ -1731,6 +1771,9 @@
                                 relVal += '<br/>' + params[i].seriesName + ' : ' + params[i].value + "人";
                             }
                             return relVal;
+
+
+                            
                         },
                     },
 
